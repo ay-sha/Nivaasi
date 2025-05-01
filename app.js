aca3d4989ce813576b4998bb083b7bbadbc829fe
Macbook <macbook@MacBook-Pro.local>
2025-05-01 16:42:23 +0600

    Schema Validation Implemented with Joi

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override')
-const ejsMate = require('ejs-mate'); 
+const ejsMate = require('ejs-mate');
 const MONGO_URL = "mongodb://127.0.0.1:27017/nivaasi";
 const Listing = require("./models/listing.js")
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+const{ listingSchema } = require('./schema.js'); 
 
-app.set('view enginee','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride("_method")); 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "public"))); 
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
     .then((res) => {
@@ -26,48 +29,75 @@ async function main() {
 app.get('/', (req, res) => {
     res.send('Root');
 })
+const validateListing =(req,res,next)=>{
+    let {err} = listingSchema.validate(req.body);
+    if(err){
+        let errMsg = err.details.map((el)=>el.message).join(","); 
+        throw new ExpressError(400,errMsg); 
+    }
+    else{
+        next(); 
+    }
+}
+
 //Index
-app.get('/listings', async (req,res)=>{
+app.get('/listings', wrapAsync( async (req, res) => {
     const allListing = await Listing.find({});
-    res.render('./listings/index.ejs',{allListing});
-})
+    res.render('./listings/index.ejs', { allListing });
+}));
 
 //New
-app.get('/listings/new',(req,res)=>{
+app.get('/listings/new', (req, res) => {
     res.render('./listings/new.ejs');
-})
+});
 
 //Show
-app.get('/listings/:id', async(req,res)=>{
-    let {id} = req.params;
+app.get('/listings/:id', wrapAsync( async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render('./listings/show.ejs',{listing}); 
-})
+    res.render('./listings/show.ejs', { listing });
+}));
+
 //Create
-app.post('/listings',async(req,res)=>{
-    let newListing = new Listing(req.body.listing);
+app.post('/listings', validateListing, wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save()
     res.redirect('/listings');
-})
-//edit
-app.get('/listings/:id/edit', async (req,res)=>{
-    let {id} = req.params;
+}));
+
+//Edit
+app.get('/listings/:id/edit', wrapAsync( async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render('./listings/edit.ejs',{listing}); 
-})
+    res.render('./listings/edit.ejs', { listing });
+}));
+
 //update
-app.put('/listings/:id',async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put('/listings/:id',validateListing, wrapAsync (async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`)
-})
+}));
+
 //delete
-app.delete('/listings/:id',async (req,res)=>{
-    let {id} = req.params;
+app.delete('/listings/:id',wrapAsync( async (req, res) => {
+    let { id } = req.params;
     let deltedListings = await Listing.findByIdAndDelete(id);
-    console.log(deltedListings); 
+    console.log(deltedListings);
     res.redirect('/listings');
-})
+}));
+
+app.all(/./, (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+//custom middleware and warpasync
+
+app.use((err, req, res, next) => {
+    let { status=500, message='Something Went Wrong' } = err;
+    // res.status(status).send(message);
+    res.status(status).render('error.ejs',{err}); 
+});
 
 
 app.listen(3000, () => {
